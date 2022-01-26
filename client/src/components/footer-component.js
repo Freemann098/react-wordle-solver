7b04985657868047616fc3be03091529a7bf5396
Chase Smith <freemann098@gmail.com>
2022-01-26 14:32:50 -0700

    Updated footer
    Fixed Github profile link
    Added Github link to project repository

--- client/src/components/footer-component.js
+++ client/src/components/footer-component.js
@@ -1,13 +1,18 @@
 import { Component } from "react";
+import githubLogo from '../assets/GitHub-Mark-Light-32px.png'
 
 class FooterComponent extends Component {
     render() {
-        const myLink = <a target="_blank" rel="noreferrer" href="https://github.com/Freemann098">Chase Smith</a>;
+        const myLink = <a target="_blank" rel="noreferrer" href="https://github.com/chasersmith">Chase Smith</a>;
         const reactLink = <a target="_blank" rel="noreferrer" href="https://reactjs.org/">React</a>;
         const nodeLink = <a target="_blank" rel="noreferrer" href="https://nodejs.org/en/">Node.js</a>;
 
         return (
-            <p style={{fontSize: "16px"}}>Made by {myLink} using {reactLink} and {nodeLink} 😁</p>
+            <div>
+                <br></br>
+                <a target="_blank" rel="noreferrer" href="https://github.com/chasersmith/react-wordle-solver"><img src={githubLogo} alt="github"></img></a>
+                <p style={{fontSize: "16px"}}>Made by {myLink} using {reactLink} and {nodeLink} 😁</p>
+            </div>
         );
     }
 }
